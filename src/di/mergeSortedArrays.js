fba4c84dc0348c07f887db135bb9fddbf874cf3c
dianavashti <diana.vashti@gmail.com>
2018-03-06 17:51:24 -0800

    Fixes bug for edge case of second array being exhausted before first.

--- src/di/mergeSortedArrays.js
+++ src/di/mergeSortedArrays.js
@@ -12,20 +12,29 @@ const mergeSortedArrays = (firstArray, secondArray) => {
   let returnArray = []
   const loopsNum = firstArray.length + secondArray.length;
 
-  // one single loop equal to the lengths of both arrays together
-  for( let i=0; i < loopsNum; i++ ){
-    if(firstArray[0] <= secondArray[0]){
-      returnArray.push(firstArray[0])
-      firstArray.shift()
+  // As long as one array has items in it we contunie merging
+  while( firstArray.length !== 0 || secondArray.length !== 0 ){
+    // check if either array is empty and just concat the other on the solution
+    // and we are done
+    if(firstArray.length === 0) {
+      returnArray = returnArray.concat(secondArray);
+      return returnArray
+    } else if(secondArray.length === 0) {
+      returnArray = returnArray.concat(firstArray);
+      return returnArray
     } else {
-      returnArray.push(secondArray[0])
-      secondArray.shift()
+      // neither array is empty
+      if(firstArray[0] <= secondArray[0]) {
+        returnArray.push(firstArray[0])
+        firstArray.shift()
+      } else {
+        returnArray.push(secondArray[0])
+        secondArray.shift()
+      }
     }
   }
-  // at this point all items of firstArray and secondArray have been shifted off
-  // and pushed to our returnArray in order. 
-  return returnArray
-
 };
 
 console.log( mergeSortedArrays( [1,4,5,8,9],[2,4,7,11,13] ) ); // [1,2,4,4,5,7,8,9,11,13]
+console.log( mergeSortedArrays( [1,4,5],[7,11,13] ) ); // [1,4,5,7,11,13]
+console.log( mergeSortedArrays( [8,9,10],[1,2,3,5,6] ) ); // [1,2,3,5,6,8,9,10]
